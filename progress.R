5a0eb996d50deffd503ddd54175cbb260a971c9a
trvinh <tran@bio.uni-frankfurt.de>
2018-03-29 19:39:44 +0200

    chap 5 finished

--- progress.R
+++ progress.R
@@ -5,19 +5,19 @@ chapter <- rep(c("01-Introduction","02-Estimation of LCA protein set",
                  "04-Distribution analysis","05-HamFAS",
                  "06-Metabolic pathway analysis",
                  "07-Discussion & Outlook"), each = 3)
-type <- rep(c("draft","refining","illustration"),5)
+type <- rep(c("draft","refined","illustration"),5)
 progress <- c(100,100,100, # chapter 01 Introduction
               100,100,95, # chapter 02 LCA estimation
-              1,1,10, # chapter 03 PhyloProfile
+              50,1,80, # chapter 03 PhyloProfile
               100,100,90, # chapter 04 Distribution analysis
-              100,30,90, # chapter 05 HamFAS
+              100,95,90, # chapter 05 HamFAS
               80,1,50, # chapter 06 pathway analysis
               1,1,1 # chapter 07 Discussion & Outlook
               )
 
 df <- as.data.frame(cbind(chapter,type,progress), stringsAsFactors = FALSE)
 df$progress <- as.numeric(df$progress)
-df$type <- factor(df$type, levels = c("draft","refining","illustration"))
+df$type <- factor(df$type, levels = c("draft","refined","illustration"))
 
 ggplot(df, aes(x = chapter, y = progress)) +
   geom_bar(aes(fill = type), stat = "identity", position = position_dodge(width = 0.7), width = 0.5) +
